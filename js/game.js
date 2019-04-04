ab780d68bd397f864215db7744c6ec4f2e2456bb
Petal Jagdeo <petaljagdeo1@gmail.com>
2019-04-04 14:18:52 +0000

    almost done

--- js/game.js
+++ js/game.js
@@ -1,46 +1,145 @@
 // NOTE - You must have a level with the name "start".  This is used as the first level in the game.
 
 var game = {
-    music: "98_Lost_Mine.mp3",
-    background_image: "intro-bg.jpg",
+    music: "",
+    background_image:"",
     levels: {
 
         start: {
-            message: "You come across a dark cave",
+            color: "#e699ff",
+            message: "Drake",
             choices: [
                 {
-                    text: "Enter the cave",
-                    nextLevel: "cave",
-                },
-
-                {
-                    text: "Keep on moving",
-                    nextLevel: "field",
+                    text: "Start here",
+                    nextLevel: "second",
                 },
             ]
         },
 
-        cave: {
-            background_image: "fire.gif",
-            music: "Final-Fantasy-7-Boss-Battle.mp3",
-            message: "You come across a fire monster or something!",
+        second: {
+            color:"#aa7710",
+            message: "As you planned to meet your friends, you see Drake in the train. What do you do?",
             choices: [
                 {
-                    text: "Start over",
+                    text: "Take a pic and go on to meet your friends",
+                    nextLevel: "open",
+                    
+                },
+                {
+                    text: "Ditch your friends and hang out with Drake",
+                    nextLevel: "leave",
+                },
+            ]
+        },
+
+        open: {
+            color:"#aa7710",
+            message: "Kudos for you being a good friend :)",
+            choices: [
+                { 
+                    text: "Yay!",
+                    nextLevel: "three",
+                },
+            ]
+        },
+        
+        leave: {
+            color:"#585858",
+            message: "You are such a fake friend that they left you. At least, you were with Drake",
+            choices: [
+                {
+                    text: "Damn",
+                    nextLevel: "three",
+                },
+            ]
+        },
+        
+        three: {
+            color:"#1a75ff",
+            message: "You made plans with Drake for a date. Where do you want to go?",
+            choices: [
+                {
+                    text: "Amusement Park",
+                    nextLevel: "quick",
+                    
+                },
+                
+                {
+                    text: "Stay in and have a movie night",
+                    nextLevel: "long",
+                    
+                },
+            ]
+        },
+        
+        quick: {
+            color:"#4d94ff",
+            message: "You had a great time and he won you a teddy bear. Cheers to having a good first date :)",
+            choices: [
+                {
+                    text: "Good for you",
+                    nextLevel: "four",
+                    
+                },
+                
+            ]
+        },
+        
+        long: {
+            color:"#505050",
+            message: "Ya had a great time and he wants to see you again ;)",
+            choices: [
+                {
+                    text: "Boy, now thats just crazy",
+                    nextLevel: "four",
+                },
+                
+            ]
+        },
+        
+        four: {
+            color:"#ff4d4d",
+            message: "However, your ex wants to get back with you. What do you do?",
+            choices: [
+                {
+                    text: "Take him back",
+                    nextLevel: "take",
+                    
+                },
+                
+                {
+                    text: "Tell him that you are seeing someone",
+                    nextLevel: "seeing",
+                    
+                },
+                
+            ]
+        },
+        
+        take: {
+            color:"#484848",
+            message: "Its all good, but you feel guilty leaving Drake. Your time with Drake is over",
+            choices: [
+                {
+                    text: ":(",
                     nextLevel: "start",
                 },
+                
             ]
         },
-
-        field: {
-            message: "Some adventurer you are...",
+        
+        seeing: {
+            color:"#ff3333",
+            message: "You do you girl! At least you got Drake :)",
             choices: [
                 {
-                    text: "Start over",
+                    text: ";)",
                     nextLevel: "start",
                 },
+                
             ]
         },
-
+        
+        
     }
 };
